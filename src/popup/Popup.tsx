18942aee599f2175407dfbbfb701fc01f55ad91c
Justin <jusheinz@aol.de>
2022-10-20 15:34:37 -0500

    init

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,7 +1,6 @@
 import { Box, Text, Button, ChakraProvider, theme } from "@chakra-ui/react";
 import React, { useEffect } from "react";
 //import "./Popup.scss";
-import { injectScript } from "../utils/src";
 import Home from "../pages/Home";
 
 //{(window as any).ethereum.getBalance("0x1F98431c8aD98523631AE4a59f267346ea31F984")}
@@ -10,7 +9,6 @@ export default function Popup() {
     // Example of how to send a message to eventPage.ts.
     chrome.runtime.sendMessage({ popupMounted: true });
   }, []);
-  injectScript();
 
   return <Home />;
 }
